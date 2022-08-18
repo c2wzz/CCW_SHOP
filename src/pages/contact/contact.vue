a9d02e72d2589383488a8a0dfe2869af0e98b71b
c2w102897 <c2w102897@zbwlkj.net>
2022-08-18 16:29:58 +0800

    添加拨打点击事件

--- src/pages/contact/contact.vue
+++ src/pages/contact/contact.vue
@@ -3,7 +3,7 @@
     <image class="img" src="https://raw.githubusercontent.com/c2wzz/CCW_SHOP/master/src/static/aboutUs.png"
       mode="scaleToFill" />
     <view class="info">
-      <view>联系电话:400-8828-8820 (点击拨打)</view>
+      <view @click="callMe">联系电话:400-8828-8820 (点击拨打)</view>
       <view>联系地址:福建省福州市闽侯县旗山万佛寺天王殿</view>
     </view>
     <map class="map" :longitude="longitude" :latitude="latitude" :markers="WanfoTemple"></map>
@@ -20,9 +20,18 @@ export default {
         {
           longitude: 119.212869,
           latitude: 25.99064,
-          iconPath: '../../static/hmlogo.png'
+          iconPath: '../../static/hmlogo.png',
+          width:50,
+          height:50
         }
       ]
+    }
+  },
+  methods:{
+    callMe(){
+      uni.makePhoneCall({
+        phoneNumber:'18649834737'
+      })
     }
   }
 
