af7a529a53d467f0cdc627e226a89bbef4d25e93
c2w102897 <c2w102897@zbwlkj.net>
2022-08-18 16:00:06 +0800

    设置地图和地图的key

--- src/pages/contact/contact.vue
+++ src/pages/contact/contact.vue
@@ -1,6 +1,15 @@
 <template>
-  <view>
-    <view>跳转</view>
+  <view class="contact">
+    <image
+      class="img"
+      src="https://raw.githubusercontent.com/c2wzz/CCW_SHOP/master/src/static/aboutUs.png"
+      mode="scaleToFill"
+    />
+    <view class="info">
+      <view>联系电话:400-8828-8820 (点击拨打)</view>
+      <view>联系地址:福建省福州市闽侯县南屿镇南井村松杭洲</view>
+    </view>
+    <map></map>
   </view>
 </template>
 
@@ -10,6 +19,18 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang="scss">
+  .contact{
+    .img{
+      width: 750rpx;
+      height: 320rpx;
+    }
+    .info{
+      view{
+        padding: 10rpx 20rpx;
+        line-height: 60rpx;
+        border-bottom: 1px solid #eee
+      }
+    }
+  }
 </style>
