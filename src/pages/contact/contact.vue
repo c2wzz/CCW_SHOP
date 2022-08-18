0b0d9bbee2ccca9426398adcc5520d89d5b11a5f
c2w102897 <c2w102897@zbwlkj.net>
2022-08-18 16:24:23 +0800

    添加联系我们页地图和坐标点

--- src/pages/contact/contact.vue
+++ src/pages/contact/contact.vue
@@ -1,36 +1,52 @@
 <template>
   <view class="contact">
-    <image
-      class="img"
-      src="https://raw.githubusercontent.com/c2wzz/CCW_SHOP/master/src/static/aboutUs.png"
-      mode="scaleToFill"
-    />
+    <image class="img" src="https://raw.githubusercontent.com/c2wzz/CCW_SHOP/master/src/static/aboutUs.png"
+      mode="scaleToFill" />
     <view class="info">
       <view>联系电话:400-8828-8820 (点击拨打)</view>
-      <view>联系地址:福建省福州市闽侯县南屿镇南井村松杭洲</view>
+      <view>联系地址:福建省福州市闽侯县旗山万佛寺天王殿</view>
     </view>
-    <map></map>
+    <map class="map" :longitude="longitude" :latitude="latitude" :markers="WanfoTemple"></map>
   </view>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      longitude: 119.212869,
+      latitude: 25.99064,
+      WanfoTemple: [
+        {
+          longitude: 119.212869,
+          latitude: 25.99064,
+          iconPath: '../../static/hmlogo.png'
+        }
+      ]
+    }
+  }
 
 }
 </script>
 
 <style lang="scss">
-  .contact{
-    .img{
-      width: 750rpx;
-      height: 320rpx;
-    }
-    .info{
-      view{
-        padding: 10rpx 20rpx;
-        line-height: 60rpx;
-        border-bottom: 1px solid #eee
-      }
+.contact {
+  .img {
+    width: 750rpx;
+    height: 320rpx;
+  }
+
+  .info {
+    view {
+      padding: 10rpx 20rpx;
+      line-height: 60rpx;
+      border-bottom: 1px solid #eee
     }
   }
+
+  .map {
+    width: 750rpx;
+    height: 750rpx;
+  }
+}
 </style>
